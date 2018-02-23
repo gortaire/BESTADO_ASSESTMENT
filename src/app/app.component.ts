382d9cccda7aeb6096edad4e9570f3620efbfee5
Giorgio Matias Gortaire Vilches <ggortaiv@everis.com>
2018-02-23 14:53:04 -0300

    BANCOESTADO Assestment App Razas de Perros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Http, Response, Headers } from '@angular/http';
+import { NgForOf } from '@angular/common';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'app';
-  results = '';
 
-  constructor(private http: HttpClient){
+export class AppComponent implements OnInit {
+
+  title = 'App Razas de Perros (by Giorgio)';
+  breeds:any = {};
+  selectedBreed = null;
+
+  constructor(private http: Http){
   }
 
   ngOnInit(): void {
-    this.
-    http.get('https://dog.ceo/api/breeds/list/all').subscribe(
+    this.http
+    .get('https://dog.ceo/api/breeds/list')
+    .map(
+      (res:Response) => res.json()
+    )
+    .subscribe(
       data => {
         console.log(data);
+        this.breeds = data.message;
       }
     );
   }
+
+  getBreedDetail(breed:string){
+    return this.http
+    .get('https://dog.ceo/api/breed/'+breed+'/images')
+    .map((res:Response) => res.json())
+    .subscribe(data => {
+      console.log(data);
+      this.selectedBreed = data.message;
+    });
+  }
+
+  onSelect(breed:string){
+    this.getBreedDetail(breed);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,9 +9,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
